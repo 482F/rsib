7f1309545943438f86929cfa9ff8e6e77cd67d47
482F <482F.git.mail@482f.net>
2023-11-12 00:00:00 +0900

    自動実行機能を追加

--- browser-extension/content.ts
+++ browser-extension/content.ts
@@ -1,13 +1,27 @@
-import { extensionMessenger } from './message.ts'
+import { port } from '../const.ts'
+import { extensionB2CMessenger, extensionC2BMessenger } from './message.ts'
 
-export function listen(
-  rawListener: Parameters<typeof extensionMessenger.createListener>[0],
+function insertScript(scriptUrl: string) {
+  const script = document.createElement('script')
+  script.src = scriptUrl
+  document.head.appendChild(script)
+}
+
+export async function content(
+  uniqueListener: Parameters<typeof extensionB2CMessenger.createListener>[0],
+  uniqueSender: Parameters<typeof extensionC2BMessenger.createSender>[0],
 ) {
-  extensionMessenger.createListener(rawListener)({
+  extensionB2CMessenger.createListener(uniqueListener)({
     'exec-order': ({ scriptUrl }) => {
-      const script = document.createElement('script')
-      script.src = scriptUrl
-      document.head.appendChild(script)
+      insertScript(scriptUrl)
     },
+  })
+  const sender = extensionC2BMessenger.createSender(uniqueSender)
+  const { scriptMap } = await sender('get-matched-script-urls', {
+    currentUrl: location.href,
+  })
+
+  Object.values(scriptMap).forEach(({ name }) => {
+    insertScript(`http://localhost:${port}/script/${name}#${Date.now()}`)
   })
 }
--- browser-extension/message.ts
+++ browser-extension/message.ts
@@ -1,10 +1,23 @@
 import { messengerCreator } from '../message.ts'
+import { Script } from '../type.ts'
 
-export const extensionMessenger = messengerCreator<
+export const extensionB2CMessenger = messengerCreator<
   ({
     type: 'exec-order'
     request: {
       scriptUrl: string
+    }
+  })
+>()
+
+export const extensionC2BMessenger = messengerCreator<
+  ({
+    type: 'get-matched-script-urls'
+    request: {
+      currentUrl: string
+    }
+    response: {
+      scriptMap: Record<string, Script>
     }
   })
 >()
