7aaca984b817817f69a5b48df3c137da43df5604
482F <482F.git.mail@482f.net>
2023-11-13 00:00:00 +0900

    background.ts に chrome に依存している部分があったので修正

--- browser-extension/background.ts
+++ browser-extension/background.ts
@@ -1,42 +1,30 @@
-/// <reference types="https://unpkg.com/chrome-types@0.1.153/index.d.ts" />
-
 import { port } from '../const.ts'
 import { websocketMessenger } from '../message.ts'
 import { Script } from '../type.ts'
 import { Message } from './ext-type.ts'
 
-function sendMessage(tabId: number, message: Message) {
-  chrome.tabs.sendMessage(tabId, JSON.stringify(message))
-}
-
 const scriptMap: Record<string, Script> = {}
 
-export function background() {
+export function background(
+  messageSender: (
+    message: Message,
+    option?: { activeTab?: boolean },
+  ) => void,
+) {
   websocketMessenger.createListener()({
     'keepalive': () => {},
-    'exec-order': async (message) => {
-      const windowId = await chrome.windows.getLastFocused().then((win) =>
-        win.id
-      )
-      if (!windowId) {
-        return
-      }
-
-      const [tab] = await chrome.tabs.query({ active: true, windowId })
-      if (!tab?.id) {
-        return
-      }
-
+    'exec-order': (message) => {
       const script = scriptMap[message.scriptName]
+      console.log({ script })
       if (!script) {
         return
       }
 
-      sendMessage(tab.id, {
+      messageSender({
         type: 'exec-order',
         scriptUrl:
           `http://localhost:${port}/script/${script.name}#${Date.now()}`,
-      })
+      }, { activeTab: true })
     },
     'update-scriptmap': (message) => {
       if (message.isInit) {
--- browser-extension/chrome/src/background.ts
+++ browser-extension/chrome/src/background.ts
@@ -1,3 +1,38 @@
-import { background } from '../../background.ts'
+/// <reference types="https://unpkg.com/chrome-types@0.1.153/index.d.ts" />
 
-background()
+import { Message } from '../../ext-type.ts'
+
+import { background } from '../../background.ts'
+import { isNonNullish } from '../../../common.ts'
+
+function sendMessage(tabIds: number[], message: Message) {
+  tabIds.forEach((tabId) =>
+    chrome.tabs.sendMessage(tabId, JSON.stringify(message))
+  )
+}
+
+background(async (body, option) => {
+  const tabIds = await (async () => {
+    if (option?.activeTab) {
+      const windowId = await chrome.windows.getLastFocused().then((win) =>
+        win.id
+      )
+      if (!windowId) {
+        return []
+      }
+
+      const [tab] = await chrome.tabs.query({ active: true, windowId })
+      if (!tab?.id) {
+        return []
+      }
+
+      return [tab.id]
+    } else {
+      const tabs = await chrome.tabs.query({})
+      return tabs.map(({ id }) => id).filter(isNonNullish)
+    }
+  })()
+  console.log({ body, option, tabIds })
+
+  sendMessage(tabIds, body)
+})
